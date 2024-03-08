ef3de5a463cead25134b0d87bc1da5adcf1ed584
Ay7753 <131199208+Ay7753@users.noreply.github.com>
2024-03-08 13:27:05 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,12 @@
+class a
+{
+  public staic void main(String ar[])
+ {
+   System.out.println("this is class A");
+     int a,b,c;
+   a=10;
+   b=20;
+ c=a+b;
+ system.out.println("sum="+c);
+ }
+}
